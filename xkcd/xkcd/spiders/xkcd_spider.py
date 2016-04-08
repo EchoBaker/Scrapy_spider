bac2e0d98535942e8c914945bcb12ef87a43fb11
Echo Baker <littleboy.echo@gmail.com>
2016-04-08 21:01:31 +0800

    Start

--- xkcd/xkcd/spiders/xkcd_spider.py
+++ xkcd/xkcd/spiders/xkcd_spider.py
@@ -3,4 +3,10 @@ import scrapy
 
 class XkcdSpider(scrapy.Spider):
     name = 'xkcd'
-    domain
+    allowed_domains = ["xkcd.com/"]
+    start_urls = ["http://xkcd.com/"]
+
+    def parse(self, response):
+        filename = response.url.split("/")[2] + '.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
