9d7d6eacdcf220a1ea9260e07b0ccb2e341891fc
Echo Baker <littleboy.echo@gmail.com>
2016-04-09 20:38:16 +0800

    ont the way

--- xkcd/xkcd/pipelines.py
+++ xkcd/xkcd/pipelines.py
@@ -4,8 +4,11 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
-
+import hashlib
+from scrapy.conf import settings
+from scrapy.http import Request
+import psycopg2
+from cStringIO import StringIO
 import scrapy
 from scrapy.pipelines.images import ImagesPipeline
 from scrapy.exceptions import DropItem
@@ -15,7 +18,7 @@ class XkcdPipelinee(ImagesPipeline):
 
     def get_media_requests(self, item, info):
         for image_url in item['link']:
-            yield scrapy.Request(image_url)
+            return scrapy.Request(image_url)
 
     def item_completed(self, results, item, info):
         image_paths = [x['path'] for ok, x in results if ok]
@@ -23,3 +26,15 @@ class XkcdPipelinee(ImagesPipeline):
             raise DropItem("Item contains no images")
         item['image_paths'] = image_paths
         return item
+
+    def convert_image(self, image, size=None):
+        buf = StringIO()
+        try:
+            image.save(buf, image.format)
+        except Exception, ex:
+            raise ImageException("Cannot process image. Error: %s" % ex)
+        return image, buf
+
+    def image_key(self, url):
+        image_guid = hashlib.sha1(url).hexdigest()
+        return 'full/%s.jpg' % (image_guid)
